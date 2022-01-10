6448b06676d270498ac943aa9ea4e62011aca258
amendahabrywork <adahabre@pmc.gov.il>
2022-01-10 15:26:12 +0200

    FormsAlert

--- src/app/employees-manage-dash/employees-add-update/employees-add-update.component.ts
+++ src/app/employees-manage-dash/employees-add-update/employees-add-update.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesAddUpdateComponent implements OnInit {
 
+  employee: any;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/employees-manage-dash/employees-manage-dash.component.ts
+++ src/app/employees-manage-dash/employees-manage-dash.component.ts
@@ -1,11 +1,14 @@
-import { Component, NgZone, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 import { MatDialog } from '@angular/material/dialog';
 import { DatePipe } from '@angular/common';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { EmployeesAddUpdateComponent } from '../employees-manage-dash/employees-add-update/employees-add-update.component';
 
 @Component({
   selector: 'app-employees-manage-dash',
@@ -13,6 +16,13 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./employees-manage-dash.component.css']
 })
 export class EmployeesManageDashComponent implements OnInit {
+
+  TABLE_DATA: any[] = [];
+  displayedColumns: string[] = [
+    'employee_id', 'first_name', 'last_name', 'gender', 'date_of_birth', 'cell_number', 'elictive', 'email', 'function_description', 'departnent_code'
+  ];
+  dataSource = new MatTableDataSource(this.TABLE_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private zone: NgZone,
     private modal: NgbModal,
@@ -22,19 +32,38 @@ export class EmployeesManageDashComponent implements OnInit {
     private _snackBar: MatSnackBar,
     private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+    searchEmployeesGroup: FormGroup;
 
-    // this.GetDepartmentsToUpdateBeds();
+  ngOnInit(): void {
+    this.searchEmployeesGroup = this.formBuilder.group({
+      EmpID: new FormControl('',null),
+      EmpFirstName: new FormControl('',null),
+      EmpLastName: new FormControl('',null),
+      Elective: new FormControl('',null),
+      On: new FormControl('',null),
+    });
+    this.GetEmployeesToUpdate();
   }
 
-  GetDepartmentsToUpdateBeds() {
-
+  GetEmployeesToUpdate() {
+    let empId = this.searchEmployeesGroup.controls['EmpID'].value;
+    let empFirstName = this.searchEmployeesGroup.controls['EmpFirstName'].value;
+    let empLastName = this.searchEmployeesGroup.controls['EmpLastName'].value;
+    let elective = this.searchEmployeesGroup.controls['Elective'].value;
+    let on = this.searchEmployeesGroup.controls['On'].value;
     this.http
       .post("http://srv-apps/wsrfc/WebService.asmx/GetEmployeesToUpdate", {
+        
       })
       .subscribe((Response) => {
-        let t = Response["d"];
+        this.dataSource = new MatTableDataSource<any>(Response["d"]);
+        this.dataSource.paginator = this.paginator;
       });
+  }
+
+  addUpdateEmployee(employee){
+    let dialogRef = this.dialog.open(EmployeesAddUpdateComponent, { disableClose: true });
+    dialogRef.componentInstance.employee = employee;
   }
 
 }
