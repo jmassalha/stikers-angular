dd13babc9d57fee2015266d07c4d9975d6002ce1
Jammalmassalha <jammalmassalha@gmail.com>
2022-01-20 09:07:33 +0200

    jammal 20012022

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -286,6 +286,7 @@ export class HeaderComponent implements OnInit {
         if (
             this.loginUserName.toLowerCase() == "jmassalha" ||
             this.loginUserName.toLowerCase() == "samer" ||
+            this.loginUserName.toLowerCase() == ("HROSHROSH").toLowerCase() ||
             this.loginUserName.toLowerCase() == "owertheim"
         ) {
             this._SHowToAdmins = true;
@@ -476,6 +477,7 @@ export class HeaderComponent implements OnInit {
             this.loginUserName.toLowerCase() == "ssarusi" ||
             this.loginUserName.toLowerCase() == "sabuhanna" ||
             this.loginUserName.toLowerCase() == "relmalem" ||
+            this.loginUserName.toLowerCase() == ("HROSHROSH").toLowerCase() ||
             this.loginUserName.toLowerCase() == "tklinger"
             ||
             this.loginUserName.toLowerCase() == "aamara"
