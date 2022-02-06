4a46866592bd33ca3b28913bb7bc915f7500a48b
Your Name <you@example.com>
2022-02-06 15:55:09 +0200

    surgeryroom

---
+++ src/app/nurses-manage-dashboard/nurses-reinforcement/nurses-reinforcement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nurses-reinforcement',
+  templateUrl: './nurses-reinforcement.component.html',
+  styleUrls: ['./nurses-reinforcement.component.css']
+})
+export class NursesReinforcementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
