55dfdb40d12bbf0b27d88d0631702f82744cc2c8
Jammalmassalha <jammalmassalha@gmail.com>
2022-10-26 08:52:46 +0300

    jammal surgery 26102022

--- src/app/surgery-control-main/surgery-control-main.component.ts
+++ src/app/surgery-control-main/surgery-control-main.component.ts
@@ -114,8 +114,8 @@ export class SurgeryControlMainComponent implements OnInit {
         }
         this.http
             .post(
-                "http://localhost:64964/WebService.asmx/GetSurgeryControlMain",
-                //"http://srv-apps-prod/RCF_WS/WebService.asmx/ServersTable",
+                //"http://localhost:64964/WebService.asmx/GetSurgeryControlMain",
+                "http://srv-apps-prod/RCF_WS/WebService.asmx/GetSurgeryControlMain",
                 {
                     type: type,
                     room: this.selectedBtn,
